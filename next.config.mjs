6dd912f6fff66bb3b70d7b6649f916f02195fd4a
Innocent Goodness <innocentgoodness009@gmail.com>
2025-10-23 00:08:37 +0100

    chore:vercel deploy big fixed

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack(config) {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      ['@react-native-async-storage/async-storage']: false,
+    };
+    return config;
+  },
 }
 
 export default nextConfig
